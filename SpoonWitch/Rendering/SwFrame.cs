dff33148f79d6a991d921016375439984431db73
Riley <waskerdu@gmail.com>
2026-08-21 23:11:55 -0700

    asprite imports

--- SpoonWitch/Rendering/SwFrame.cs
+++ SpoonWitch/Rendering/SwFrame.cs
@@ -4,10 +4,11 @@ using SpoonWitch.Utils;
 
 namespace SpoonWitch.Rendering;
 
-public readonly struct SwFrame(SwTextureStore texture, ErRect2 sourceRect)
+public readonly struct SwFrame(SwTextureStore texture, ErRect2 sourceRect, double duration = 0.125)
 {
     public readonly SwTextureStore Texture = texture;
     public readonly ErRect2 SourceRect = sourceRect;
+    public readonly double Duration = duration;
     public void Draw(ErVec2 position, ErVec2? origin = null, double angle = 0, bool hFlip = false, bool vFlip = false)
     {
         Texture.DefaultTexture.Draw(position, SourceRect.Size, SourceRect, origin, angle, hFlip, vFlip);
@@ -31,5 +32,10 @@ public readonly struct SwFrame(SwTextureStore texture, ErRect2 sourceRect)
         if(!tiles.TryGetTiles(out var tileRects, Enumerable.Range(firstFrame, lastFrame - firstFrame + 1))) return false;
         frames = [..tileRects.Select(t => new SwFrame(texture, t))];
         return true;
+    }
+    public static bool TryGetRects(out IEnumerable<ErRect2> rects, SwTextureStore texture, ErVec2 frameSize, int firstFrame, int lastFrame, ErVec2? tileOffset = null, ErVec2? tilePadding = null)
+    {
+        SwTileSplitter tiles = new(texture.DefaultTexture.Size, frameSize, tileOffset, tilePadding);
+        return tiles.TryGetTiles(out rects, Enumerable.Range(firstFrame, lastFrame - firstFrame + 1));
     }
 }
