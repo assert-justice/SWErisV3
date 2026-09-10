2ca4e44649fbd49aefc673fc1c85541c25823c11
Riley <waskerdu@gmail.com>
2026-09-09 17:41:52 -0700

    start pickup ent

---
+++ SpoonWitch/Game/Entity/SwPickup.cs
@@ -0,0 +1,35 @@
+using ErisPhysics2D.Collider;
+using Prion.Node;
+using SpoonWitch.Game.Entity.Component;
+using SpoonWitch.Game.Map.Collision;
+
+namespace SpoonWitch.Game.Entity;
+
+public class SwPickup : SwEntity, ISwEntity<SwPickup>
+{
+    public static byte TypeId => 3;
+    private static SwPickup? _Primary;
+    private static SwPickup? _Secondary;
+    public static SwPickup Primary => _Primary ??= new();
+    public static SwPickup Secondary => _Secondary ??= new();
+    protected override byte GetTypeId => TypeId;
+    public SwPickup()
+    {
+        SwAreaComponent area = new(this, "spoon_hurtbox", 4, new(32,32));
+        area.Area.OnBodyEnterFn = OnEnter;
+        RegisterComponent(area);
+    }
+    public override void Ready()
+    {
+        base.Ready();
+        PriDict command = [];
+        EntProps.Props.TrySet("ent_offer_item", command);
+    }
+    private static void OnEnter(SwColliderArea area, int bodyId, ErColliderBody body)
+    {
+        if(!SwGame.TryGetEntProps(area.ParentId, out var pickupProps)) return;
+        if(!SwGame.TryGetEntProps(body.ParentId, out var targetProps)) return;
+        if(!pickupProps.Props.TryGet("spoon_damage", out PriNode spoonDamage)) return;
+        targetProps.AddCommand(spoonDamage);
+    }
+}
