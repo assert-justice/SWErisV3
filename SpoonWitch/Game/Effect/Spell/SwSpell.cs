b34f344f00f9c543bb54d772306e19a7e41ea0c8
Riley <waskerdu@gmail.com>
2026-09-18 14:55:07 -0700

    start on prototype system

---
+++ SpoonWitch/Game/Effect/Spell/SwSpell.cs
@@ -0,0 +1,20 @@
+namespace SpoonWitch.Game.Effect.Spell;
+
+public class SwSpell
+{
+    public double ManaCost = 10;
+    private bool _IsActive = false;
+    public bool IsActive => _IsActive;
+    public virtual void Begin()
+    {
+        _IsActive = true;
+    }
+    public virtual void End()
+    {
+        _IsActive = false;
+    }
+    public virtual void Update()
+    {
+        //
+    }
+}
