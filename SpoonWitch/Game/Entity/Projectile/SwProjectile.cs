e316cea61ef671ca608336f516e89a821edefd5f
Riley <waskerdu@gmail.com>
2026-09-22 00:29:41 -0700

    spell projectiles are working

--- SpoonWitch/Game/Entity/Projectile/SwProjectile.cs
+++ SpoonWitch/Game/Entity/Projectile/SwProjectile.cs
@@ -4,68 +4,78 @@ using ErisMath;
 using Prion.Node;
 using SpoonWitch.Game.Entity.Component;
 using SpoonWitch.Rendering;
+using SpoonWitch.Utils;
 
 namespace SpoonWitch.Game.Entity.Projectile;
 
 public class SwProjectile : SwEntity
 {
     private ErTexture? Texture;
-    private SwParticles2D? ImpactParticles;
-    private SwParticles2D? FlyingParticles;
+    // private SwAnimation? Animation;
+    private SwParticleComponent? ImpactParticles;
+    // private SwParticles2D? FlyingParticles;
     private uint CollisionMask = 0;
     public ErVec2 Velocity;
     public bool Piercing = false;
     private bool IsAlive = true;
     public override int RenderLayer => 3;
-    private readonly SwAreaComponent Hurtbox;
-    public SwProjectile()
+    private SwAreaComponent Hurtbox = null!;
+    protected override void SetProps(PriNode props)
     {
+        base.SetProps(props);
+        if(Props.TryGet("mask", out uint u)) CollisionMask = u;
+        if(Props.TryGet("piercing", out bool b)) Piercing = b;
+        Velocity = SwPrion.GetVec2(Props.Get("velocity"));
+    }
+    public override void Init()
+    {
+        base.Init();
         Hurtbox = new(this, "hurtbox", 4, new(14,14), enabled:true, onBodyEnter: OnEnterHurtbox);
         RegisterComponent(Hurtbox);
-            // if(!ErTexture.TryFromPath("game_data/entities/actors/player/images/bella_sling_ammo_shot.png", out Texture)) ErEngine.LogError("bad projectile texture path");
-    }
-    public override void Ready()
-    {
-        base.Ready();
-        Props.TryGet("x_velocity", out double xVel);
-        Props.TryGet("y_velocity", out double yVel);
-        Velocity = new(xVel, yVel);
-        if(Props.TryGet("collision_mask", out uint i)) CollisionMask = i;
-        if(Props.TryGet("texture_filepath", out string texture_filepath))
-        {
-            if(!ErTexture.TryFromPath(texture_filepath, out Texture)) ErEngine.LogWarning("bad projectile texture path");
-        }
+        TryGetBody();
         if(Props.TryGet("impact_particles", out PriDict pData))
         {
-            if(!SwParticles2D.TryFromData(out ImpactParticles, pData)) ErEngine.LogWarning("bad projectile impact particles at path");
+            if(!SwParticles2D.TryFromData(out var particles, pData)) ErEngine.LogWarning("bad projectile impact particles at path");
+            else
+            {
+                ImpactParticles = new(this, "impact_particles", particles);
+                RegisterComponent(ImpactParticles);
+            }
         }
         if(Props.TryGet("flying_particles", out pData))
         {
-            if(!SwParticles2D.TryFromData(out FlyingParticles, pData)) ErEngine.LogWarning("bad projectile flying particles at path");
+            if(!SwParticles2D.TryFromData(out var particles, pData)) ErEngine.LogWarning("bad projectile flying particles at path");
+            else RegisterComponent(new SwParticleComponent(this, "flying_particles", particles));
         }
+    }
+    private bool TryGetBody()
+    {
+        if(!Props.TryGet("body", out PriDict body)) return false;
+        if(body.TryGet("texture_filepath", out string texture_filepath)) ErTexture.TryFromPath(texture_filepath, out Texture);
+        else if(body.TryGet("ase_data", out PriDict dict))
+        {
+            if(!dict.TryGet("name", out string name)) return ErEngine.LogWarning("no name found for projectile ase animation");
+            if(!SwAseImporter.TryFromPriData(out var aseImporter, dict.Get("filepath"))) return false;
+            if(!aseImporter.TryGetAnimation(out var animation, name)) return ErEngine.LogWarning("invalid name for projectile ase animation");
+            // Animation = animation;
+            SwSprite sprite = new("body");
+            sprite.AddAnimation(animation);
+            RegisterComponent(new SwSpriteComponent(this, sprite));
+        }
+        return true;
     }
     private void Impact()
     {
         IsAlive = false;
         Hurtbox.Enabled = false;
-        ImpactParticles?.Emitting = true;
+        ImpactParticles?.Particles.Emitting = true;
     }
     public override void Update()
     {
         base.Update();
-        if(ImpactParticles is not null)
-        {
-            ImpactParticles.Origin = Position;
-            ImpactParticles.Update(SwGame.DeltaTime);
-        }
-        if(FlyingParticles is not null)
-        {
-            FlyingParticles.Origin = Position;
-            FlyingParticles.Update(SwGame.DeltaTime);
-        }
         if (!IsAlive)
         {
-            if(ImpactParticles is null || ImpactParticles.LiveParticles == 0) QueueFree();
+            if(ImpactParticles is null || ImpactParticles.Particles.LiveParticles == 0) QueueFree();
             return;
         }
         Position += Velocity * SwGame.DeltaTime;
@@ -83,8 +93,6 @@ public class SwProjectile : SwEntity
             var pos = ErMath.Lerp(Position, nextState.Position, SwGame.FrameWeight) - Texture.Size * 0.5;
             Texture.Draw(pos);
         }
-        ImpactParticles?.Draw(SwGame.FrameDuration);
-        FlyingParticles?.Draw(SwGame.FrameDuration);
     }
     private void OnEnterHurtbox(SwEntity entity)
     {
